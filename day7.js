849397c497f76b8b6352b195e3590bdbccf37f84
Matt MacAulay <mac@twg.io>
2017-06-26 08:36:10 -0400

    day 7 part 2

--- day7.js
+++ day7.js
@@ -1,14 +1,23 @@
 const fs = require('fs')
 const lines = fs.readFileSync('day7.txt', { encoding: 'utf8' }).split('\n')
+// example TLS lines
 // const lines = [
 //   'abba[mnop]qrst', // yes
 //   'abcd[bddb]xyyx', // no
 //   'aaaa[qwer]tyui', // no
 //   'ioxxoj[asdfgh]zxcvbn' // yes
 // ]
+// example SSL lines
+// const lines = [
+//   'aba[bab]xyz', // yes
+//   'xyx[xyx]xyx', // no
+//   'aaa[kek]eke', // yes
+//   'zazbz[bzb]cdb' // yes
+// ]
 let count = 0
 lines.forEach(line => {
-  if (line && supportsTLS(line)) {
+  // if (line && supportsTLS(line)) {
+  if (line && supportsSSL(line)) {
     count++
   }
 })
@@ -61,6 +70,62 @@ function supportsTLS (line) {
     console.log('NO:', line, 'abba:', hasAbba, 'brackets:', hasAbbaInBracketsContext)
   }
   return hasAbba && !hasAbbaInBracketsContext
+}
+
+function isABA (chars) {
+  return chars[0] === chars[2] &&
+         chars[0] !== chars[1]
+}
+
+function isBAB (aba, chars) {
+  return aba[0] === chars[1] &&
+         aba[1] === chars[0]
+}
+
+function hasBAB (aba, collectedBABs) {
+  for (let i = 0; i < collectedBABs.length; i++) {
+    const bab = collectedBABs[i]
+    if (isBAB(aba, bab)) return true
+  }
+}
+
+function supportsSSL (line) {
+  let bracketsContext = false
+  const collected = []
+  const collectedABAs = []
+  const collectedBABs = []
+
+  const chars = line.split('')
+  for (let i = 0; i < chars.length; i++) {
+    const char = chars[i]
+    collected.push(char)
+
+    const lastThreeChars = chars.slice(i - 2, i + 1)
+    if (lastThreeChars[2] === '[') {
+      bracketsContext = true
+      continue
+    } else if (lastThreeChars[2] === ']') {
+      bracketsContext = false
+      continue
+    }
+    if (collected.length < 3) continue // nothing to do here
+    if (isABA(lastThreeChars)) {
+      if (bracketsContext) {
+        collectedBABs.push(lastThreeChars)
+      } else {
+        collectedABAs.push(lastThreeChars)
+      }
+    }
+  }
+
+  for (let i = 0; i < collectedABAs.length; i++) {
+    const aba = collectedABAs[i]
+    if (hasBAB(aba, collectedBABs)) {
+      console.log('has BAB:', line)
+      count++
+      break
+    }
+  }
 }
 
 console.log('count:', count)
